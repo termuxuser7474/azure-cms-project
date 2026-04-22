1cb4ba04df190be244c957d5c144f80fdd6fa4d3
Syed Rahim S <syedsyedsyed008@gmail.com>
2026-04-22 14:40:12 +0530

    Added ProxyFix and Logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,10 +7,20 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
+# Configure logging for the application
+app.logger.setLevel(logging.INFO)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.INFO)
+app.logger.addHandler(streamHandler)
+
+# Tell Flask it is behind a proxy to ensure HTTPS is sent to Microsoft Entra ID
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
